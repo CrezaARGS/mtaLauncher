f14d328c9b20142385edb4006c1b0359ddfa8ae3
Creza#1453 <107704959+CrezaARGS@users.noreply.github.com>
2022-08-09 19:29:41 +0300

    Update Form1.Designer.cs

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -38,7 +38,7 @@ namespace MTADefaultLancher
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Join";
+            this.button1.Text = "Giriş Yap";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
